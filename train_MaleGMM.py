8bf92153eb0aa59decb6180b279bbc6557f5fbb2
vaaiibhav <vaaiibhav@live.com>
2019-10-07 20:42:16 +0530

    Dataset included

---
+++ train_MaleGMM.py
@@ -0,0 +1,42 @@
+
+import os
+import _pickle as cPickle
+import numpy as np
+from scipy.io.wavfile import read
+from sklearn.mixture import GMM 
+import python_speech_features as mfcc
+from sklearn import preprocessing
+import warnings
+warnings.filterwarnings("ignore")
+
+def get_MFCC(sr,audio):
+    features = mfcc.mfcc(audio,sr, 0.025, 0.01, 13,appendEnergy = False)
+    features = preprocessing.scale(features)
+    return features
+
+
+#path to training data
+source   = "train_data\\male\\"         
+#path to save trained model
+dest     = "Model\\"             
+
+files    = [os.path.join(source,f) for f in os.listdir(source) if 
+             f.endswith('.wav')] 
+features = np.asarray(());
+
+for f in files:
+    sr,audio = read(f)
+    print("File: ",f)
+    vector   = get_MFCC(sr,audio)
+    if features.size == 0:
+        features = vector
+    else:
+        features = np.vstack((features, vector))
+
+gmm = GMM(n_components = 8, n_iter = 200, covariance_type='diag',
+        n_init = 3)
+gmm.fit(features)
+picklefile = f.split("\\")[-2].split(".wav")[0]+".gmm"
+
+# model saved as .gmm
+cPickle.dump(gmm,open(dest + picklefile,'wb'))
